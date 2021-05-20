5f24bfbbf2b9c6f8c73e5a78b358e778bf9fff9b
Sunny Pahuja <sunny.pahuja89@gmail.com>
2021-05-19 19:46:57 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Lists from "./Lists";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      alldata: [],
+      singledata: {
+        title: "",
+        author: ""
+      }
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  getLists = () => {
+    fetch("http://localhost:5000/posts")
+      .then(res => res.json())
+      .then(result =>
+        this.setState({ alldata: result, loading: false })
+      )
+      .catch(console.log);
+  }
+
+
+  render() {
+    const listTable = this.state.loading ? (
+      <span>Loading Data...please be patience.</span>
+    ) : (<Lists alldata={this.state.alldata} />
+    );
+  
+    return (
+      <div className="container">
+        <span className="title-bar">
+          <button type="button"
+            className="btn btn-primary"
+            onClick={this.getLists}>
+            Get Lists
+        </button>
+        </span>
+        {listTable}
+      </div>
+    )
+
+  }
 }
-
 export default App;
